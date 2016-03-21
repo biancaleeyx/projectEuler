7accea8a914b4d037413a28935dce5349c5acf59
biancaleeyx <biancaleeyx@gmail.com>
2016-03-21 11:14:06 +0800

    problem 20, non-ASCII character in description

---
+++ 020_factorial_digit_sum.py
@@ -0,0 +1,22 @@
+"""
+Project Euler Problem 20: Factorial digit sum
+
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+"""
+
+import numpy as np
+
+# Set number.
+number = 100
+
+digitSum = 0
+
+for digitString in str(np.math.factorial(number)):
+	digitSum += int(digitString)
+
+print(digitSum)
