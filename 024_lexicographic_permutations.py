8482f56f45abff89c1ded22b676a7b21b8ef7db4
biancaleeyx <biancaleeyx@gmail.com>
2016-03-28 02:38:01 +0800

    permutation number minus 1

--- 024_lexicographic_permutations.py
+++ 024_lexicographic_permutations.py
@@ -13,9 +13,18 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 import numpy as np
 
 # Set variables.
-digits = 10
+digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+digits = np.sort(np.array(digits))
 permutationNum = 1000000
 
+# Number of permutations of n numbers = n!
+permutationNum -= 1
+permutation = ''
+while len(digits) != 0:
+	digit = (permutationNum//np.math.factorial(len(digits)-1))
+	permutation += str(digits[digit])
+	permutationNum = permutationNum % np.math.factorial(len(digits)-1)
+	digits = np.delete(digits, np.where(digits==digits[digit]))
 
-
+print(permutation)
 print(__file__ + " has finished running.")
