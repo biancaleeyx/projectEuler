f8810d96f76dac05ccb9b702690ae3f42c2fed7b
biancaleeyx <biancaleeyx@gmail.com>
2016-05-08 00:17:39 +0800

    031: still brute force, inserted initial empty combination

--- 031_coin_sums.py
+++ 031_coin_sums.py
@@ -27,23 +27,27 @@ total = 200
 coins = np.array([1, 2, 5, 10, 20, 50, 100, 200])
 
 # Find list of combinations, unique.
-combinationList = []
+combinationList = [[]]
 
 # Create list of maximum number of coins of each type.
 coinsMax = []
 for coin in coins:
-	coinsMax.append(total/coin)
+	coinsMax.append(total//coin)
 coinsMax = np.array(coinsMax)
 print("Variables set.")
 
 # Create brute force combination of numbers of coins of each type.
 for coinType, coinMax in zip(coins, coinsMax):
+	print("Processing coin of value {}...".format(coinType))
 	for combination in combinationList:
-		for coinNum in range(coinMax+1):
-			combinationTemp = copy.deepcopy(combination)
-			combinationTemp.append(coinNum)
-			combinationList.append(combinationTemp)
-		combinationList.delete(combination)
+		if len(combination) > np.where(coins==coinType)[0][0]:
+			break
+		else:
+			for coinNum in range(coinMax+1):
+				combinationTemp = copy.deepcopy(combination)
+				combinationTemp.append(coinNum)
+				combinationList.append(combinationTemp)
+			combinationList.remove(combination)
 print("Generation of combination list complete. \nNow calculating number of ways...")
 
 ways = 0
@@ -54,4 +58,4 @@ for combination in combinationList:
 print("Number of ways to form £2: {}.".format(ways))
 
 timeEnd = time.clock()
-print("Time taken: {} minutes, {} seconds.".format((timeEnd-timeStart)//60, (timeEnd-timeStart)%60))
+print("Time taken: {:0.0f} minutes, {:0.0f} seconds.".format((timeEnd-timeStart)//60, (timeEnd-timeStart)%60))
